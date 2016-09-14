e187274ad60a95d0cf26ae963a524398372c9b40
Annie Sonna and Ryan Vinyard <annie+ryan@>
2016-09-14 16:35:13 -0700

    added arraylists and ids

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,47 @@
+import java.time.LocalDateTime;//java universal date stamp//
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task {
   private String mDescription;
+  private boolean mCompleted; //marks task as complated or not//
+  private LocalDateTime mCreatedAt; //creates an object of the LocalDateTime//
+  private static List<Task> instances = new ArrayList<Task>();//Will hold a list of all objects created with the constructor//
+  private int mId; //declares Id as an int//
 
-  public Task(String description) {
-    mDescription = description;
+  public Task(String description) { //Constructor. Takes the description as an argument//
+    mDescription = description; //Takes description from the argument passed//
+    mCompleted = false;//Auto marks as not completed//
+    mCreatedAt = LocalDateTime.now();//Finds the current date//
+    instances.add(this);//adds this current object to the list called "instances"//
+    mId = instances.size();//Assigns an ID property which is related to the order it's added to the List.//
   }
+
   public String getDescription() {
     return mDescription;
+  }
+
+  public boolean isCompleted() {
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
+  }
+
+  public static List<Task> all() {
+    return instances;//returns the list called "instances"//
+  }
+
+  public static void clear () {
+    instances.clear();
+  }//clears the list called "instances" clear() is static because clearing all Task objects will occur on the class-level not the instance-level//
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Task find(int id) {
+    return instances.get(id - 1);//static method to locate a Task using its mId property. This method is static because it must sift through all Task objects to find the specific one we're seeking. And, because it is static, we will call it on the entire class, like this: Task.find().//
   }
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,5 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+
 
 public class TaskTest {
   @Test
@@ -13,5 +15,44 @@ public class TaskTest {
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
   }
-  
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
+  }
+
+  @Test
+   public void all_returnsAllInstancesOfTask_true() {
+     Task firstTask = new Task("Mow the lawn");
+     Task secondTask = new Task("Buy groceries");
+     assertEquals(true, Task.all().contains(firstTask));
+     assertEquals(true, Task.all().contains(secondTask));
+   }
+
+   @Test
+   public void clear_emptiesAllTasksFromArrayList_0() {
+     Task myTask = new Task("Mow the lawn");
+     Task.clear();
+     assertEquals(Task.all().size(), 0);
+ }
+   @Test
+   public void getId_tasksInstantiateWithAnID_1() {
+     Task.clear();  // Remember, the test will fail without this line! We need to empty leftover Tasks from previous tests!//
+     Task myTask = new Task("Mow the lawn");
+     assertEquals(1, myTask.getId());
+ }
+ //include Task.clear() at the beginning of the test. Again, this is very important. The list containing all Tasks will not automatically empty itself between each test.//
+     @Test
+    public void find_returnsTaskWithSameId_secondTask() {
+      Task firstTask = new Task("Mow the lawn");
+      Task secondTask = new Task("Buy groceries");
+      assertEquals(Task.find(secondTask.getId()), secondTask);
+    }
 }
