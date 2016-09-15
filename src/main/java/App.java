317e167c5c19a5580d6c534fc7a803499ac5b50d
GeorginaVanDort <georginavandort@gmail.com>
2016-09-14 20:22:32 -0700

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,8 @@
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
-import java.util.List;
-import java.util.ArrayList;
-
 
 public class App {
   public static void main(String[] args) {
@@ -14,24 +11,27 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("tasks", request.session().attribute("tasks"));
       model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());//render template//
+
+    get("tasks/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());//render template//
+
+    get("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", Task.all());//place the ArrayList of all Tasks into the model,
+      model.put("template", "templates/tasks.vtl");//makes available to the template tasks.vtl//
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/tasks", (request, response) -> {
+    post("/tasks", (request,response) -> {//where the form submits to <form action="/tasks" method="post">//
       Map<String, Object> model = new HashMap<String, Object>();
-
-      ArrayList<Task> tasks = request.session().attribute("tasks");
-      if (tasks == null) {
-        tasks = new ArrayList<Task>();
-        request.session().attribute("tasks", tasks);
-      }
-
-      String description = request.queryParams("description");
-      Task newTask = new Task(description);
-      tasks.add(newTask);
-
+      String description = request.queryParams("description");//gathers info from the form//
+      Task newTask = new Task(description);//creates task//
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
